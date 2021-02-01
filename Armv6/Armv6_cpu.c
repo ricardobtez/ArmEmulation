7ec016836fe80c995d39fcb14a00dfec57d60856
Ricardo Benitez <benitezc.ricardo@gmail.com>
2021-02-01 17:35:40 -0600

    [Code] decodeInstruction finished
    - decodeInstruction finished without testing
    
    Signed-off-by: Ricardo Benitez <benitezc.ricardo@gmail.com>

--- Armv6/Armv6_cpu.c
+++ Armv6/Armv6_cpu.c
@@ -19,26 +19,26 @@ typedef enum InstructionGroup_t
 struct InstructionPattern_t
 {
     uint8_t pattern;
-    uint8_t size;
+    uint8_t mask;
     InstructionGroup_t group;
 }InstructionPattern_t;
 
 const struct InstructionPattern_t InstructionPattern[] =
 {
-    {0x00, 2, SHIFT_GROUP},                 // 0b00xx_xx
-    {0x60, 3, LOAD_STORE_SINGLE_GROUP},     // 0b011x_xx
-    {0x80, 3, LOAD_STORE_SINGLE_GROUP},     // 0b100x_xx
-    {0x41, 4, LOAD_STORE_SINGLE_GROUP},     // 0b0101_xx
-    {0xB0, 4, MISCELANIOUS_GROUP},          // 0b1011_xx
-    {0xD0, 4, CONDITIONAL_BRANCH_GROUP},    // 0b1101_xx
-    {0xA0, 5, GENERATE_RELATIVE_ADD_GROUP}, // 0b1010_0x
-    {0xA8, 5, GENERATE_RELATIVE_ADD_GROUP}, // 0b1010_1x
-    {0x48, 5, LOAD_STORE_SINGLE_GROUP},     // 0b0100_1x
-    {0xC0, 5, LOAD_STORE_MULTIPLE_GROUP},   // 0b1100_0x
-    {0xC8, 5, LOAD_STORE_MULTIPLE_GROUP},   // 0b1100_1x
-    {0xE0, 5, UNCONDITIONAL_BRANCH_GROUP},  // 0b1101_xx
-    {0x80, 6, DATA_PROCESSING_GROUP},       // 0b0100_00
-    {0x44, 6, SPECIAL_DATA_GROUP}           // 0b0100_01
+    {0x00, 0xC0, SHIFT_GROUP},                 // 0b00xx_xx
+    {0x60, 0xE0, LOAD_STORE_SINGLE_GROUP},     // 0b011x_xx
+    {0x80, 0xE0, LOAD_STORE_SINGLE_GROUP},     // 0b100x_xx
+    {0x41, 0xF0, LOAD_STORE_SINGLE_GROUP},     // 0b0101_xx
+    {0xB0, 0xF0, MISCELANIOUS_GROUP},          // 0b1011_xx
+    {0xD0, 0xF0, CONDITIONAL_BRANCH_GROUP},    // 0b1101_xx
+    {0xA0, 0xF8, GENERATE_RELATIVE_ADD_GROUP}, // 0b1010_0x
+    {0xA8, 0xF8, GENERATE_RELATIVE_ADD_GROUP}, // 0b1010_1x
+    {0x48, 0xF8, LOAD_STORE_SINGLE_GROUP},     // 0b0100_1x
+    {0xC0, 0xF8, LOAD_STORE_MULTIPLE_GROUP},   // 0b1100_0x
+    {0xC8, 0xF8, LOAD_STORE_MULTIPLE_GROUP},   // 0b1100_1x
+    {0xE0, 0xF8, UNCONDITIONAL_BRANCH_GROUP},  // 0b1101_xx
+    {0x80, 0xFC, DATA_PROCESSING_GROUP},       // 0b0100_00
+    {0x44, 0xFC, SPECIAL_DATA_GROUP}           // 0b0100_01
 };
 
 /// \brief
@@ -114,15 +114,18 @@ void execute(CPU* cpu, const Instruction* instruction)
 InstructionGroup_t decodeInstruction(const Instruction* instruction)
 {
     InstructionGroup_t group = INVALID_INSTRUCTION_GROUP;
-    uint16_t it = 0;
-    const uint16_t MAX_OPCODE_SIZE = 
-        InstructionPattern[ sizeof(InstructionPattern) /sizeof(InstructionPattern_t) - 1 ].size + 1;
+    uint16_t it = sizeof(InstructionPattern) /sizeof(InstructionPattern_t) - 1;
+    uint8_t masked_instruction = 0x0000;
 
-    while (it < MAX_OPCODE_SIZE)
+    while (it >= 0 && INVALID_INSTRUCTION_GROUP == group)
     {
-        // Verify bit by bit from the MSB to the LSB the bit selection
-        
-        ++it;
+        masked_instruction = ((uint8_t)(instruction->rawData >> 8)) & InstructionPattern[it].mask;
+
+        if (masked_instruction == InstructionPattern[it].pattern)
+        {
+            group = InstructionPattern[it].group;
+        }
+        --it;
     }
 
     return group;
